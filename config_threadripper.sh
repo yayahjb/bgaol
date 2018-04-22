d4e6d7d0f4342fb3700c76b1142336f95626839b
Herbert J. Bernstein <yayahjb@gmail.com>
2018-04-21 23:04:55 -0400

    Provide threadripper config and fix extra comma in IO -- HJB

---
+++ config_threadripper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+host_name=flops.arcib.org:8084
+HTTPDSERVER=${host_name};export HTTPDSERVER
+SEARCHURL=http://${host_name}/bgaol;export SEARCHURL
+CGIPATH=http://${host_name}/cgi-bin/;export CGIPATH
+BINDEST=/var/www/cgi-bin; export BINDEST
+CGIBIN=$BINDEST; export CGIBIN
+FC=gfortran; export FC
+FFLAGS="-O3"; export FFLAGS
+CGIBINEXT=/cgi-bin; export CGIBINEXT
+CGIMETHOD=POST; export CGIMETHOD
+HTDOCS=/var/www/html/bgaol; export HTDOCS
+HTDOCSEXT=/bgaol; export HTDOCS
+
